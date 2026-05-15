8c4d6c086bafbc849d1139f6d694e7925279f801
raider001 <furnell.daniel@gmail.com>
2026-05-15 13:18:05 +0930

    feat: simplify NotificationServiceSettingsPanel by removing test notification functionality

--- ReviewToolApplication/src/main/java/com/kalynx/serverlessreviewtool/ui/mainpanels/settingspanel/NotificationServiceSettingsPanel.java
+++ ReviewToolApplication/src/main/java/com/kalynx/serverlessreviewtool/ui/mainpanels/settingspanel/NotificationServiceSettingsPanel.java
@@ -1,8 +1,6 @@
 package com.kalynx.serverlessreviewtool.ui.mainpanels.settingspanel;
 
 import com.kalynx.serverlessreviewtool.configuration.SettingsManager;
-import com.kalynx.serverlessreviewtool.notifications.SystemNotifier;
-import com.kalynx.serverlessreviewtool.swingextensions.themedcomponents.ThemedButton;
 import com.kalynx.serverlessreviewtool.swingextensions.themedcomponents.ThemedLabel;
 import com.kalynx.serverlessreviewtool.swingextensions.themedcomponents.ThemedPanel;
 import com.kalynx.serverlessreviewtool.swingextensions.themedcomponents.ThemedTextField;
@@ -11,8 +9,7 @@ import com.kalynx.serverlessreviewtool.utils.Validator;
 import net.miginfocom.swing.MigLayout;
 
 /**
- * Panel for configuring the automatic notification service URL and
- * for testing desktop notification delivery.
+ * Panel for configuring the automatic notification service URL.
  */
 public class NotificationServiceSettingsPanel extends ThemedPanel {
 
@@ -20,7 +17,6 @@ public class NotificationServiceSettingsPanel extends ThemedPanel {
 
     private final ThemedLabel urlLabel = new ThemedLabel("Service URL:");
     private final ThemedTextField urlTextField = new ThemedTextField(30);
-    private final ThemedButton testNotificationButton = new ThemedButton("Test Notification");
 
     /**
      * Creates a new {@code NotificationServiceSettingsPanel}.
@@ -31,16 +27,14 @@ public class NotificationServiceSettingsPanel extends ThemedPanel {
         this.settingsManager = settingsManager;
         configureLayout();
         setupValidation();
-        setupListeners();
         loadDefaults();
     }
 
     private void configureLayout() {
-        setLayout(new MigLayout("", "[][grow][]", "[][]"));
+        setLayout(new MigLayout("", "[][grow]", "[][]"));
         setBorder(ThemedTitledBorder.create("Automatic Notification Service"));
         add(urlLabel, "cell 0 0, align right");
         add(urlTextField, "cell 1 0, growx");
-        add(testNotificationButton, "cell 2 0");
     }
 
     private void setupValidation() {
@@ -50,20 +44,10 @@ public class NotificationServiceSettingsPanel extends ThemedPanel {
         );
     }
 
-    private void setupListeners() {
-        testNotificationButton.addActionListener(_ -> onTestNotification());
-    }
-
     private void loadDefaults() {
         urlTextField.setText(settingsManager.getSettings().getNotificationServiceUrl());
     }
 
-    private void onTestNotification() {
-        SystemNotifier.getInstance().sendNotification(
-            "Review Tool",
-            "System notifications are working correctly."
-        );
-    }
 
     private Validator.ValidationResult validateUrl(String urlString) {
         if (urlString.isEmpty()) {
